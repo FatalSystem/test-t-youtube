2237d8474ff54efde0bc29d117fe2b951d919fb5
FatalSystem <kzavali567@gmail.com>
2025-05-27 17:47:00 +0300

    s3

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -3,6 +3,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "@/lib/graphql/client";
 
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
